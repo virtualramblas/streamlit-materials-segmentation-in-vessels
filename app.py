a6d9e8b5356e7d0d20c0b8ba36feca6e097c7166
virtualramblas <guglielmo.iozzia@gmail.com>
2021-03-28 23:11:01 +0100

    Implemented CNN model loading.

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
 import cv2
 import numpy as np
 import streamlit as st
+import torch
 from PIL import Image
+
+import CategoryDictionary as CatDic
+import FCN_NetModel as FCN
+
+@st.cache(allow_output_mutation=True)
+def load_cnn_model(UseGPU=False):
+    trained_model_path = 'model/TrainedModelWeiht1m_steps_Semantic_TrainedWithLabPicsAndCOCO_AllSets.torch'
+    net = FCN.Net(CatDic.CatNum) 
+    if UseGPU==True:
+        net.load_state_dict(torch.load(trained_model_path))
+    else:
+        net.load_state_dict(torch.load(trained_model_path, map_location=torch.device('cpu')))
+        
+    return net
+
+model = load_cnn_model()
 
 st.sidebar.info("Inference settings")
 use_gpu = st.sidebar.checkbox('Use GPU')
@@ -13,7 +30,6 @@ if uploaded_file is not None:
     st.sidebar.text('Input image:')
     st.sidebar.image(uploaded_image, width=288)
     predict_button = st.sidebar.button('Predict')
-    clean_button = st.sidebar.button('Clean')
     
 st.write(
     """ # Detecting, segmenting and classifying materials inside mostly transparent vessels  """
